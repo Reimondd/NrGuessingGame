5abff6b447a0cec00ce6d180429a03c4121ffc6d
Reimondd <81254920+Reimondd@users.noreply.github.com>
2021-04-18 18:06:41 +0200

    Add files via upload

---
+++ NrGuessingGame.py
@@ -0,0 +1,27 @@
+from random import randint
+import os
+import time
+def main():
+    print("Number Guessing game (You only have 3 tries) \n")
+
+    secretNumber = randint(1, 5)
+    gs = 0
+    gl = 3
+    while gs < gl:
+        UIn = int(input("Guess the number in between 1 and 5: "))
+        gs += 1
+        if UIn == secretNumber:
+            print("You guessed the number!")
+            time.sleep(2)
+            os.system('cls')
+            return main()
+        elif gs == 3:
+            print("You went over the limit :/ ")
+            time.sleep(1)
+            os.system('cls')
+            return main()
+        else:
+            print("No")
+
+
+main()
